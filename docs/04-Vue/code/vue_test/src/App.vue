40916a5000e4c2344218ba4bb5c16bec07f69060
zmengyuan <zmengyuan@126.com>
2021-11-21 23:14:24 +0800

    [尚硅谷Vue]61-62 脚手架初试成功

--- docs/04-Vue/code/vue_test/src/App.vue
+++ docs/04-Vue/code/vue_test/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<School></School>
+		<Student></Student>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	//引入组件 汇总所有的组件
+	import School from './components/School.vue'
+	import Student from './components/Student.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name:'App',
+		components:{
+			School,
+			Student
+		}
+	}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- docs/04-Vue/code/vue_test/src/main.js
+++ docs/04-Vue/code/vue_test/src/main.js
@@ -1,8 +1,17 @@
+/*
+该文件是整个项目的入口文件
+*/
+//引入Vue
 import Vue from 'vue'
+//引入App，它是所有组件的父组件
 import App from './App.vue'
 
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
+//创建vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:'#app',
+  //下面这行代码一会解释，完成了这个功能，将App组件放入容器中
+	render: h => h(App),
+})
