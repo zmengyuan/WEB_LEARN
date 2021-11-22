08424562548fcb0092a561221711a523151fbf93
zmengyuan <zmengyuan@126.com>
2021-11-22 22:24:58 +0800

    [尚硅谷] 属性值写死

--- docs/04-Vue/code/vue_test/src/App.vue
+++ docs/04-Vue/code/vue_test/src/App.vue
@@ -1,35 +1,20 @@
 <template>
-  <div>
-      <h1 v-text="msg" ref="title"></h1>
-		<button ref="btn" @click="showDom">点我输出上方的DOM元素</button>
-		<School ref="sch"/>
-      
-  </div>
+	<div>
+		<Student/>
+        <hr>
+        <!-- 注意三个组件是各自的 -->
+        <!--
+        <Student/>
+        <hr>
+        <Student/> -->
+	</div>
 </template>
 
 <script>
-    import School from './components/School'
+	import Student from './components/Student'
 
-    export default {
-        name :'App',
-        data() {
-            return{
-                msg:'欢迎学习vue'
-            }
-        },
-        components: {
-            School
-        },
-        methods : {
-            showDom() {
-                console.log(this.$refs.title) //真实DOM元素
-				console.log(this.$refs.btn) //真实DOM元素
-                console.log(this.$refs.sch) //School组件的实例对象（vc）
-            }
-        }
-    }
+	export default {
+		name:'App',
+		components:{Student}
+	}
 </script>
-
-<style>
-
-</style>
---
+++ docs/04-Vue/code/vue_test/src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<h2>学生姓名：{{name}}</h2>
+		<h2>学生性别：{{sex}}</h2>
+		<h2>学生年龄：{{age}}</h2>
+		<button>尝试修改收到的年龄</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Student',
+		data() {
+			console.log(this)
+			return {
+				msg:'我是一个尚硅谷的学生',
+                age:'18',
+                name:'张三',
+                sex:'男'
+			}
+		}	
+	}
+</script>
