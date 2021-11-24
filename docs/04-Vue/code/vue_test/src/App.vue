109492a7bb7b56691fcb42fed2c474df87eed885
zmengyuan <zmengyuan@126.com>
2021-11-24 23:04:57 +0800

    [尚硅谷Vue] 79 todo 本地存储

--- docs/04-Vue/code/vue_test/src/App.vue
+++ docs/04-Vue/code/vue_test/src/App.vue
@@ -21,11 +21,7 @@
 		data() {
 			return {
 				//由于todos是MyHeader组件和MyFooter组件都在使用，所以放在App中（状态提升）
-				todos:[
-					{id:'001',title:'抽烟',done:true},
-					{id:'002',title:'喝酒',done:false},
-					{id:'003',title:'开车',done:true}
-				]
+				todos:JSON.parse(localStorage.getItem('todos')) || []
 			}
 		},
 		methods: {
@@ -55,7 +51,15 @@
 					return !todo.done
 				})
 			}
-		}
+		},
+		watch: {
+			todos:{
+				deep:true,
+				handler(value){
+					localStorage.setItem('todos',JSON.stringify(value))
+				}
+			}
+		},
 	}
 </script>
 
