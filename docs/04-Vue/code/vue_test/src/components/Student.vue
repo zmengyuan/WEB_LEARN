68c1428f6133df4c6c45609f9a44826a89b40ea4
zmengyuan <zmengyuan@126.com>
2022-08-20 17:14:54 +0800

    80vue 自定义事件的绑定

---
+++ docs/04-Vue/code/vue_test/src/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="student">
+		<h2>学生姓名：{{name}}</h2>
+		<h2>学生性别：{{sex}}</h2>
+		
+		<button @click="sendStudentlName">把学生名给App</button>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Student',
+		data() {
+			return {
+				name:'张三',
+				sex:'男'
+			
+			}
+		},
+		methods: {
+			sendStudentlName() {
+				// 促发atguigu事件
+				this.$emit('atguigu',this.name,1,2)
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.student {
+		background-color: pink;
+		padding: 5px;
+		margin-top: 30px;
+	}
+</style>
