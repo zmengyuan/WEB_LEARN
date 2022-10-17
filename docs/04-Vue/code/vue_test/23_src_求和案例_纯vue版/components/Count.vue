61c4a0846e74088d5d31dc0a3ab1692a29b14327
zmengyuan <zmengyuan@126.com>
2022-10-17 10:28:17 +0800

    [vue]基础106 求和案例——纯vue版本

---
+++ docs/04-Vue/code/vue_test/23_src_求和案例_纯vue版/components/Count.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<h1>当前求和为：{{sum}}</h1>
+		<!-- 这里写.number是因为不写的话n收集到的数据是字符串类型
+			还有一种解决方案，就是把option里面的value="1" 改为:value="1"，这样引号里面就是js表达式
+		-->
+		<select v-model.number="n">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+		</select>
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+		<button @click="incrementOdd">当前求和为奇数再加</button>
+		<button @click="incrementWait">等一等再加</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Count',
+		data() {
+			return {
+				n:1, //用户选择的数字
+				sum:0 //当前的和
+			}
+		},
+		methods: {
+			increment(){
+				this.sum += this.n
+			},
+			decrement(){
+				this.sum -= this.n
+			},
+			incrementOdd(){
+				if(this.sum % 2){
+					this.sum += this.n
+				}
+			},
+			incrementWait(){
+				setTimeout(()=>{
+					this.sum += this.n
+				},500)
+			},
+		},
+	}
+</script>
+
+<style lang="css">
+	button{
+		margin-left: 5px;
+	}
+</style>
