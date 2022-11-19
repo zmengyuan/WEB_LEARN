6e8aac669b839a95591e05d88e1ccdb085ec895c
zmengyuan <zmengyuan@126.com>
2022-11-19 14:24:22 +0800

    [html udemy]140

--- docs/01-01-Udemy课程/07-Omnifood-Desktop/js/script.js
+++ docs/01-01-Udemy课程/07-Omnifood-Desktop/js/script.js
@@ -3,6 +3,17 @@ console.log('Hello World');
 const yearEl = document.querySelector(".year");
 yearEl.textContent = new Date().getFullYear();
 
+
+//Make mobile nav
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+})
+
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
