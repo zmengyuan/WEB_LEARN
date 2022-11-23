aa9bfcf4996c6c95e1de5b2307fb8a44e6816369
zmengyuan <zmengyuan@126.com>
2022-11-23 17:31:10 +0800

    [js udemy]086-Resetting the game

--- docs/03-Udemy课程/07-Pig-Game/script.js
+++ docs/03-Udemy课程/07-Pig-Game/script.js
@@ -13,15 +13,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0]; //total score
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing
+// Starting conditions
+const init = function () {
+  scores = [0, 0]; //total score
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 
 
 const switchPlayer = function () {
@@ -77,3 +89,5 @@ btnHold.addEventListener("click", function () {
   }
 
 });
+
+btnNew.addEventListener("click", init);
