aa7ba110348ed2b2d5af2796930814ef8e18ef2e
zmengyuan <zmengyuan@126.com>
2022-11-25 17:41:30 +0800

    [js udemy]106-Rest Pattern And Parameters

--- docs/03-Udemy课程/09-Data-Structures-Operators/script.js
+++ docs/03-Udemy课程/09-Data-Structures-Operators/script.js
@@ -48,6 +48,10 @@ const restaurant = {
     );
 
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 /*
@@ -153,7 +157,7 @@ restaurant.orderDelivery({
 });
 console.log("==============================================");
 */
-
+/*
 ///////////////////////////////////////
 // The Spread Operator (...)
 
@@ -210,3 +214,45 @@ const restaurantCopy = {
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+*/
+
+///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const {
+  sat,
+  ...weekdays
+} = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
