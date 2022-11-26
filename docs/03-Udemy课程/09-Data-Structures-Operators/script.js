8873969834536c084657a1ea67fa11b456f8c979
zmengyuan <zmengyuan@126.com>
2022-11-26 23:46:22 +0800

    [js udemy]117-Maps:Fundamentals

--- docs/03-Udemy课程/09-Data-Structures-Operators/script.js
+++ docs/03-Udemy课程/09-Data-Structures-Operators/script.js
@@ -566,7 +566,7 @@ for (const [key, value] of Object.entries(game.odds)) {
   console.log(`Odd of ${str} : ${value}`);
 }
 */
-
+/*
 ///////////////////////////////////////
 // 116-Sets
 const ordersSet = new Set([
@@ -602,3 +602,37 @@ console.log(
 );
 
 console.log(new Set('jonasschmedtmann').size);
+*/
+
+///////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
