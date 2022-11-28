45f80ceb21966821720c7fe3ed3e85df66bcf326
zmengyuan <zmengyuan@126.com>
2022-11-28 12:19:39 +0800

    [js udemy]132-Functions Returning Functions

--- docs/03-Udemy课程/10-Functions/script.js
+++ docs/03-Udemy课程/10-Functions/script.js
@@ -57,7 +57,7 @@ newPassport(jonas);
 checkIn(flight, jonas);
 */
 
-
+/*
 ///////////////////////////////////////
 // Functions Accepting Callback Functions
 const oneWord = function (str) {
@@ -86,3 +86,25 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+
+///////////////////////////////////////
+// Functions Returning Functions
+// 这里已经涉及闭包了
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
