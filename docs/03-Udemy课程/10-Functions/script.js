9e0d4d93d3f02c335d9f6a2b2fcaed31b86f9a02
zmengyuan <zmengyuan@126.com>
2022-11-28 11:05:15 +0800

    [js udemy]128-Default Parameters

--- docs/03-Udemy课程/10-Functions/script.js
+++ docs/03-Udemy课程/10-Functions/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bookings = [];
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //SET DEFAULT ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 1;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  };
+  console.log(booking);
+  bookings.push(booking)
+};
+
+createBooking("LH123");
+createBooking("LH123", undefined, 1000);
