072365192865630e2fa65a2d2cb740aa0ba44e18
zmengyuan <zmengyuan@126.com>
2022-12-01 19:47:05 +0800

    [js udemy]144-Looping Arrays: Foreach

--- docs/03-Udemy课程/11-Arrays-Bankist/script.js
+++ docs/03-Udemy课程/11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 
 /*
 /////////////////////////////////////////////////
@@ -109,7 +109,7 @@ console.log([...arr, ...arr2]);
 console.log(letters.join(' - '));
 */
 
-
+/*
 ///////////////////////////////////////
 // 143 The new at Method
 const arr = [23, 11, 64];
@@ -123,3 +123,31 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+*/
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
