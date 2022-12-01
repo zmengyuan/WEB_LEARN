5b73b8d626763de7f22d03c634c403f608c0e9f0
zmengyuan <zmengyuan@126.com>
2022-12-01 19:53:55 +0800

    [js udemy]145-Foreach With Maps And Sets

--- docs/03-Udemy课程/11-Arrays-Bankist/script.js
+++ docs/03-Udemy课程/11-Arrays-Bankist/script.js
@@ -65,11 +65,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+
 
 
 
@@ -125,6 +121,7 @@ console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
 */
 
+/*
 ///////////////////////////////////////
 // Looping Arrays: forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -151,3 +148,24 @@ movements.forEach(function (mov, i, arr) {
 // 1: function(450)
 // 2: function(400)
 // ...
+*/
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
