19de6bb55e77a362d94c6987cfd27689dcef9428
zmengyuan <zmengyuan@126.com>
2022-12-02 15:55:42 +0800

    [js udemy]150-The Map Method

--- docs/03-Udemy课程/11-Arrays-Bankist/script.js
+++ docs/03-Udemy课程/11-Arrays-Bankist/script.js
@@ -209,6 +209,7 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+/*
 const Julia = [3, 5, 2, 12, 7];
 const Kate = [4, 1, 15, 8, 3];
 const checkDogs = function (dogsJulia, dogsKate) {
@@ -234,3 +235,31 @@ const checkDogs = function (dogsJulia, dogsKate) {
 
 };
 checkDogs(Julia, Kate);
+*/
+
+///////////////////////////////////////
+// 150-The map Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
