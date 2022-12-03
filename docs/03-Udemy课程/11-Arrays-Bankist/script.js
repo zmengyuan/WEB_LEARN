68d24b0c4be0976dc6cfb5a54a4d8e3c6b675a80
zmengyuan <zmengyuan@126.com>
2022-12-03 19:24:13 +0800

    [js udemy]162-Flat And FlatMap

--- docs/03-Udemy课程/11-Arrays-Bankist/script.js
+++ docs/03-Udemy课程/11-Arrays-Bankist/script.js
@@ -548,7 +548,7 @@ console.log(account);
 */
 
 
-
+/*
 ///////////////////////////////////////
 // some and every
 console.log(movements);
@@ -571,3 +571,25 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+///////////////////////////////////////
+// 162 flatES2019 and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
