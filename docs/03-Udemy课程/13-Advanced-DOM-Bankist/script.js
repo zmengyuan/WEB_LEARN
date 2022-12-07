c2978237e1ddd945c8b7161a7dba1cd6c45f15e4
zmengyuan <zmengyuan@126.com>
2022-12-07 20:45:26 +0800

    [js udemy]184-Project: Bankist Website

--- docs/03-Udemy课程/13-Advanced-DOM-Bankist/script.js
+++ docs/03-Udemy课程/13-Advanced-DOM-Bankist/script.js
@@ -8,7 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  // 因为 open account 是一个超链接标签，它的href="#"，所以点击它会默认到网页头部，执行下面方法可以阻止这项默认行动
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener('click', openModal);
+})
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
