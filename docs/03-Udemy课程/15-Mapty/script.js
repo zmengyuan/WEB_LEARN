3de2d6eb8c07ec8ccd239e39b19b6ad7f6827546
zmengyuan <zmengyuan@126.com>
2022-12-19 21:45:02 +0800

    [js udemy]233-Displaying A Map Using Leaflet Library

--- docs/03-Udemy课程/15-Mapty/script.js
+++ docs/03-Udemy课程/15-Mapty/script.js
@@ -19,6 +19,23 @@ if (navigator.geolocation) {
       longitude
     } = position.coords;
     console.log(`latitude:${latitude},longitude:${longitude}`);
+
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+    // 瓦片 开放街道地图，这里的url可以改变地图外观
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+
+    L.tileLayer(
+      'https://api.mapbox.com/styles/v1/kengqiangxia/cj5jbah540hlj2rpgh3xptiek/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2VuZ3FpYW5neGlhIiwiYSI6ImNqNWpiMWZ5ZTIxYzgyd3BrYTN2NDN5aXEifQ.bqY03lR_2LZ0fttPOJ4jyw', {
+        attribution: '铿锵侠leaflet教程, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+      }).addTo(map);
+
+    L.marker(coords).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
   }, function () {
     alert("Could not get your position")
   });
