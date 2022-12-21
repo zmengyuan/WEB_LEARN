6fa6d6a6f596d085fb0b5805296a520d363963e0
zmengyuan <zmengyuan@126.com>
2022-12-21 21:20:52 +0800

    [js udemy]258-The Event Loop In Practice

--- docs/03-Udemy课程/16-Asynchronous/script.js
+++ docs/03-Udemy课程/16-Asynchronous/script.js
@@ -214,6 +214,7 @@ TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 
 */
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/api/${lat},${lng}?geoit=json`)
     .then(response => {
@@ -241,3 +242,19 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+///////////////////////////////////////
+// The Event Loop in Practice
+
+// timer会最后执行！！
+console.log('Test start');
+setTimeout(() => {
+  console.log('0 sec timer');
+}, 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 10000000; i++) {}
+  console.log(res);
+});
+console.log('Test end');
