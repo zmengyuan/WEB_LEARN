e68da8b8b4e2827a854c09dd29ca0650effe5f11
zmengyuan <zmengyuan@126.com>
2022-12-21 21:44:44 +0800

    [js udemy]259-Building A Simple Promise

--- docs/03-Udemy课程/16-Asynchronous/script.js
+++ docs/03-Udemy课程/16-Asynchronous/script.js
@@ -246,7 +246,7 @@ whereAmI(-33.933, 18.474);
 
 ///////////////////////////////////////
 // The Event Loop in Practice
-
+/*
 // timer会最后执行！！
 console.log('Test start');
 setTimeout(() => {
@@ -258,3 +258,54 @@ Promise.resolve('Resolved promise 2').then(res => {
   console.log(res);
 });
 console.log('Test end');
+*/
+
+///////////////////////////////////////
+// Building a Simple Promise
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win');
+    } else {
+      reject(new Error('You lost your money'));
+    }
+  }, 2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.errpr(err));
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));s
