da65141e912928aacbada2cb5b316a0d246c7f3c
zmengyuan <zmengyuan@126.com>
2022-12-22 14:00:25 +0800

    [js udemy] 261-Coding Challenge 2，自己多练习！！

--- docs/03-Udemy课程/16-Asynchronous/script.js
+++ docs/03-Udemy课程/16-Asynchronous/script.js
@@ -317,6 +317,7 @@ Promise.reject(new Error('Problem!')).catch(x => console.error(x));
 //   position => console.log(position),
 //   err => console.log(err)
 // );
+/*
 console.log('Getting position');
 
 const getPosition = function () {
@@ -356,3 +357,48 @@ const whereAmI = function () {
     .catch(err => console.error(`${err.message} 馃挜`));
 };
 btn.addEventListener('click', whereAmI);
+*/
+
+// 261 challenge 2
+const imgContainer = document.querySelector('.images');
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loader');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loader');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => {
+    console.error(err);
+  });
