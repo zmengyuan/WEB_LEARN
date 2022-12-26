47d405fad8c052a3f56e62ecc43e6cbfef5d0876
zmengyuan <zmengyuan@126.com>
2022-12-26 20:22:06 +0800

    273-Top-Level Await(ES2022)

--- docs/03-Udemy课程/17-Modern-JS-Modules-Tooling/script.js
+++ docs/03-Udemy课程/17-Modern-JS-Modules-Tooling/script.js
@@ -22,3 +22,30 @@ console.log(totalPrice);
 import { cart } from './shoppingCart.js';
 
 console.log(cart);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+console.log('Start fetching');
+// 这实际上阻止了整个模块的执行
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+console.log('Something');
+
+// 假如有异步动作，需要返回一些数据
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost); //实际上这是一个Promise(pending)
+
+// Not very clean 方式一
+lastPost.then(last => console.log(last));
+
+// 如果有await，就更简单的获取了
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
