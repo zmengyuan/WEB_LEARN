9dac161c6c0d255d8d8f8342db4a3dbff0411bfe
zmengyuan <zmengyuan@126.com>
2023-02-06 20:44:07 +0800

    [重构]移除format变量

--- docs/21-重构/chapter1/index.js
+++ docs/21-重构/chapter1/index.js
@@ -66,26 +66,30 @@ function volumeCreditsFor(aPerformance) {
   return result;
 }
 
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     //print line for this order
-    result += `${playFor(perf).name}: ${format(accountFor(perf) / 10)} (${
+    result += `${playFor(perf).name}: ${usd(accountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += accountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
